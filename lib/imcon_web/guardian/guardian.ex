5b38b4a9524e4354e4907492bbd623c6ba9b9fb5
Pavel Zachesa <zachesa@ya.ru>
2019-02-12 11:07:46 +1000

    Update guardian.ex

--- lib/imcon_web/guardian/guardian.ex
+++ lib/imcon_web/guardian/guardian.ex
@@ -1,13 +1,26 @@
 defmodule ImconWeb.Guardian do
   use Guardian, otp_app: :imcon
+  alias Imcon.Auth
+
+  def current_user(conn) do
+    ImconWeb.Guardian.Plug.current_resource(conn)
+  end
 
   def subject_for_token(user, _claims) do
-    {:ok, to_string(user.id)}
+    # You can use any value for the subject of your token but
+    # it should be useful in retrieving the resource later, see
+    # how it being used on `resource_from_claims/1` function.
+    # A unique `id` is a good subject, a non-unique email address
+    # is a poor subject.
+        {:ok, to_string(user.id)}
   end
 
   def resource_from_claims(%{"sub" => id}) do
-    case Imcon.Auth.get_user!(id) do
-      nil -> {:error, :resource_not_found}
+    # Here we'll look up our resource from the claims, the subject can be
+    # found in the `"sub"` key. In `above subject_for_token/2` we returned
+    # the resource id so here we'll rely on that to look it up.
+        case Auth.get_user(id) do
+      nil -> {:error, "Unknown resource type"}
       user -> {:ok, user}
     end
   end
